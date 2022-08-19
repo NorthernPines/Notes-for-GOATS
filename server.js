e5579de7ba49668b11199ac80e88b5556cd1d8eb
Northern Pines <traviswalborn8@gmail.com>
2022-08-19 01:16:16 -0400

    added get for /api/notes

--- server.js
+++ server.js
@@ -63,6 +63,18 @@ app.post('/api/notes', (req, res) => {
             res.status(500).json('Error in saving Note');
         }
     })
+    
+    app.get('/api/notes', (req, res) => {
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Successfully retrieved notes');
+                notes = JSON.parse(data);
+                res.send(notes);
+            } 
+        })
+    })
 
     app.get('*', (req, res) =>
       res.sendFile(path.join(__dirname, '/public/index.html'))
